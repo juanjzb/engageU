52adfb7c9712540d1c01cc24c0d2d1dce2a70772
mastergamer1309 <josiaszeledon20@gmail.com>
2023-05-13 17:45:53 -0600

    a

--- js/josias.js
+++ js/josias.js
@@ -4,9 +4,7 @@ function personalInformation () {
   
     const h1PI = document.createElement('h1')
     const h1PItext = document.createTextNode('Agreguen contenido')
-    const boton1 = document.createElement("button")
-    const boton1_text = document.createTextNode
-    
+        
     h1PI.appendChild(h1PItext)
     mainContent.appendChild(h1PI)
   }
