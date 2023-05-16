263182c8141d6ad7cc6036d431873616b37f8e29
Juan Jzb <jj_extreme93@rocketmail.com>
2023-05-16 10:25:30 -0600

    volvi a reasignar los temas

--- js/jarey.js
+++ js/jarey.js
@@ -168,29 +168,100 @@ btnDragAndDropGame.addEventListener('click', e => {
   }
 })
 
-function PresentContinuous () {
-  //Code Here
-}
-function EverydayExpressions () {
-  //Code Here
-}
-function ShortQuestions () {
-  //Code Here
+/* Boton de Personal Information */
+
+function personalInformation () {
+  const mainContent = document.getElementById('main-content')
+  mainContent.innerHTML = ''
+
+  const h1PI = document.createElement('h1')
+  const h1PItext = document.createTextNode('Agreguen contenido')
+
+  h1PI.appendChild(h1PItext)
+  mainContent.appendChild(h1PI)
 }
 
-const btnPresentContinuous = document.getElementById('btnPresentContinuous')
-const btnEverydayExpressions = document.getElementById('btnEverydayExpressions')
-const btnShortQuestions = document.getElementById('btnShortQuestions')
+//////////////////////////////////////////////////////////////////
+/* Boton de Greetings */
 
-btnPresentContinuous.addEventListener('click', e => {
+function greetings () {
+  const mainContent = document.getElementById('main-content')
+  mainContent.innerHTML = ''
+
+  const h1G = document.createElement('h1')
+  const h1GText = document.createTextNode('Agreguen contenido 2')
+
+  h1G.appendChild(h1GText)
+  mainContent.appendChild(h1G)
+}
+
+////////////////////////////////////////////////////////////////////
+/* Boton de Introducing MySelf Vocabulary */
+function mySelf () {
+  const mainContent = document.getElementById('main-content')
+  mainContent.innerHTML = ''
+
+  const h1MySelf = document.createElement('h1')
+  const h1MySelf_text = document.createTextNode('Agreguen contenido 3')
+
+  h1MySelf.appendChild(h1MySelf_text)
+  mainContent.appendChild(h1MySelf)
+}
+
+/////////////////////////////////////////////////////////////////////
+/* Boton de Names And Titles */
+function namesAndTitles () {
+  const mainContent = document.getElementById('main-content')
+  mainContent.innerHTML = ''
+
+  const h1NamesAndTtiles = document.createElement('h1')
+  const h1NamesAndTtiles_text = document.createTextNode('Agreguen contenido 4')
+
+  h1NamesAndTtiles.appendChild(h1NamesAndTtiles_text)
+  mainContent.appendChild(h1NamesAndTtiles)
+}
+
+///////////////////////////////////////////////////////////////////////
+/* Boton de Aplphabet */
+
+function alphabet () {
+  const mainContent = document.getElementById('main-content')
+  mainContent.innerHTML = ''
+
+  const h1Alphabet = document.createElement('h1')
+  const h1Alphabet_text = document.createTextNode('Agreguen contenido 5')
+
+  h1Alphabet.appendChild(h1Alphabet_text)
+  mainContent.appendChild(h1Alphabet)
+}
+
+const btnPersonalInformation = document.getElementById('btnPersonalInformation')
+const btnGreetings = document.getElementById('btnGreetings')
+const btnIntroduceMySelf = document.getElementById('btnIntroduceMySelf')
+const btnNamesAndTitles = document.getElementById('btnNamesAndTitles')
+const btnAlphabet = document.getElementById('btnAlphabet')
+
+btnPersonalInformation.addEventListener('click', e => {
   e.preventDefault()
-  PresentContinuous()
+  personalInformation()
 })
-btnEverydayExpressions.addEventListener('click', e => {
+
+btnGreetings.addEventListener('click', e => {
   e.preventDefault()
-  EverydayExpressions()
+  greetings()
 })
-btnShortQuestions.addEventListener('click', e => {
+
+btnIntroduceMySelf.addEventListener('click', e => {
   e.preventDefault()
-  ShortQuestions()
+  mySelf()
+})
+
+btnNamesAndTitles.addEventListener('click', e => {
+  e.preventDefault()
+  namesAndTitles()
+})
+
+btnAlphabet.addEventListener('click', e => {
+  e.preventDefault()
+  alphabet()
 })
