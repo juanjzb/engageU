2e4cb67c20b24d9fce7824ca82e6e2a15546ef48
Victor Samuel <vs1133183@gmail.com>
2023-05-16 12:16:00 -0600

    information present simple

--- js/victor.js
+++ js/victor.js
@@ -3,11 +3,30 @@ function PresentContinuous () {
   mainContent.innerHTML = ''
 
   const h1 = document.createElement('h1')
-  const textH1 = document.createTextNode('Topic: Present Continuous')
-
+  h1.className = 'text-center text-success'
+  const textH1 = document.createTextNode('Present Continuous')
   h1.appendChild(textH1)
-
   mainContent.appendChild(h1)
+
+  const paragrafh = document.createElement('p')
+  const txtUno = document.createTextNode('The present continuous verb tense indicates that an action or condition is happening now, frequently,and may continue into the future. It generally refers to situations or actions that are being carried out at the moment and, as in Spanish, learning this tense is essential to have fluency and expression in any conversation in English.')
+  paragrafh.appendChild(txtUno)
+  mainContent.appendChild(paragrafh)
+
+  const paragrafhDos = document.createElement('h3')
+  const txtDos = document.createTextNode('The Present Continuous Formula:')
+  paragrafhDos.appendChild(txtDos)
+  mainContent.appendChild(paragrafhDos)
+
+  const paragrafhTres = document.createElement('p')
+  const txtTres = document.createTextNode(' to be [am, is, are] + verb [present participle]')
+  paragrafhTres.appendChild(txtTres)
+  mainContent.appendChild(paragrafhTres)
+
+
+
+
+
 }
 function EverydayExpressions () {
   //Code Here
