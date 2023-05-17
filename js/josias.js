1800686a87b444501b7c4d7e249d7c73f39b21a0
mastergamer1309 <josiaszeledon20@gmail.com>
2023-05-16 23:05:56 -0600

    agregue a la unidad 3

--- js/josias.js
+++ js/josias.js
@@ -3,31 +3,160 @@ function QuestionsAndConversations() {
   mainContent.innerHTML = '';
 
   const h1 = document.createElement('h1');
-  const h1_text = document.createTextNode('Questions And Conversations');
+  const h1_text = document.createTextNode('Unit III- Where are you from?');
   h1.appendChild(h1_text);
   h1.className = 'text-center text-primary';
   mainContent.appendChild(h1);
 
-  const contenido = document.createElement('p');
-  const contenido_text = document.createTextNode('¡Hola perras!');
-  contenido.classList.add('fs-4');
+  const contenido = document.createElement('h3');
+  const contenido_text = document.createTextNode('Cities and countries');
   contenido.classList.add('text-justify');
   contenido.appendChild(contenido_text);
   mainContent.appendChild(contenido);
 
-  const image = document.createElement('img');
-  image.src = './assets/img/Questions and conversations/Screenshot 2023-05-16 122332.png';
-  image.alt = 'Descripción de la imagen';
-  image.style.border = '2px solid black'; // Agrega el marco alrededor de la imagen
-  mainContent.appendChild(image);
+  const countries = [
+    'Argentina',
+    'Brazil',
+    'Canada',
+    'China',
+    'Colombia',
+    'India',
+    'Japan',
+    'Mexico',
+    'South Korea',
+    'Turkey',
+  ];
 
-  const audio = document.createElement('audio');
-  audio.src = './assets/img/Questions and conversations/y2mate.com - Efecto de sonido Yamete Kudasai xd.mp3';
-  audio.controls = true;
-  audio.style.marginTop = '10px'; // Agrega espacio superior entre la imagen y el audio
-  audio.style.border = '2px solid black'; // Agrega el borde al elemento de audio
-  mainContent.appendChild(audio);
+  const countryList = document.createElement('ul');
+  countryList.className = 'country-list';
+
+  countries.forEach((country) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = country;
+    countryList.appendChild(listItem);
+  });
+
+  mainContent.appendChild(countryList);
+
+  const activities = [
+    { question: 'Delhi and Mumbai are in _______.', answer: 'India' },
+    { question: 'Shanghai is in _______.', answer: 'China' },
+    { question: 'Tokyo is in _______.', answer: 'Japan' },
+    { question: 'Sao Paulo and Rio are in _______.', answer: 'Brazil' },
+    { question: 'Seoul and Daejeon are in _______.', answer: 'South Korea' },
+    { question: 'Buenos Aires is in _______.', answer: 'Argentina' },
+    { question: 'Vancouver and Ottawa are in _______.', answer: 'Canada' },
+    { question: 'Istanbul is in _______.', answer: 'Turkey' },
+  ];
+
+  const activityList = document.createElement('ol');
+  activityList.className = 'activity-list';
+
+  activities.forEach((activity, index) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = activity.question;
+    activityList.appendChild(listItem);
+
+    const inputContainer = document.createElement('div');
+    inputContainer.className = 'input-container';
+    listItem.appendChild(inputContainer);
+
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.id = `answer-${index}`;
+    inputContainer.appendChild(input);
+
+    const resultText = document.createElement('div');
+    resultText.id = `result-${index}`;
+    resultText.className = 'result-text';
+    inputContainer.appendChild(resultText);
+
+    input.addEventListener('keypress', (event) => {
+      if (event.key === 'Enter') {
+        checkAnswer(index, activity.answer);
+      }
+    });
+  });
+
+  mainContent.appendChild(activityList);
+
+  const conversationHeader = document.createElement('h3');
+  conversationHeader.textContent = "Complete the conversations with am, 'm, are, 're, is, or 's";
+  mainContent.appendChild(conversationHeader);
+
+  const conversation = document.createElement('div');
+  conversation.className = 'conversation';
+
+  const conversationText = document.createElement('p');
+  conversationText.innerHTML = `1. A:  --- you and your family from South Korea? B: No, we --- not. We --- from China. A: Oh, so you --- from China. B: Yes, I --- . I --- from Shanghai.`;
+  conversation.appendChild(conversationText);
+
+  const conversationInputs = [
+    { placeholder: 'Enter', answer: 'Are' },
+    { placeholder: 'Enter', answer: "are" },
+    { placeholder: 'Enter', answer: "are" },
+    { placeholder: 'Enter', answer: 'are' },
+    { placeholder: 'Enter', answer: 'am' },
+    { placeholder: 'Enter', answer: "am" },
+  ];
+  conversationInputs.forEach((inputData, index) => {
+    const input = document.createElement('input');
+input.type = 'text';
+input.placeholder = inputData.placeholder;
+input.id =`conversation-answer-${index}`;
+conversation.appendChild(input);
+const resultText = document.createElement('div');
+resultText.id = `conversation-result-${index}`;
+resultText.className = 'result-text';
+conversation.appendChild(resultText);
+input.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    checkConversationAnswer(index, inputData.answer);
+  }
+});
+});
+mainContent.appendChild(conversation);
 }
+
+function checkAnswer(index, answer) {
+  document.getElementById(`answer-${index}`).value.toLowerCase();
+const correctAnswer = answer.toLowerCase();
+const resultText = document.getElementById(`result-${index}`);
+if (userInput === correctAnswer) {
+  resultText.textContent = 'Correcto';
+resultText.classList.remove('incorrect');
+resultText.classList.add('correct');
+}else{
+  resultText.textContent = 'Incorrecto';
+resultText.classList.remove('correct');
+resultText.classList.add('incorrect');
+}
+}
+
+function checkConversationAnswer(index, answer) {
+  const userInput = document.getElementById(`conversation-answer-${index}`).value.toLowerCase();
+const correctAnswer = answer.toLowerCase();
+const resultText = document.getElementById(`conversation-result-${index}`);
+if (userInput === correctAnswer) {  
+  resultText.textContent = 'Correcto';
+resultText.classList.remove('incorrect');
+resultText.classList.add('correct');
+}else{
+  resultText.textContent = 'Incorrecto';
+resultText.classList.remove('correct');
+resultText.classList.add('incorrect');
+}
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
