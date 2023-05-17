7eba0412376180eea1635619f4a08f7894c55ae7
Victor Samuel <vs1133183@gmail.com>
2023-05-16 23:06:14 -0600

    Se agrego mas informacion

--- js/victor.js
+++ js/victor.js
@@ -29,10 +29,19 @@ function PresentContinuous () {
   paragrafhCuatro.appendChild(txtCuatro)
   mainContent.appendChild(paragrafhCuatro)
 
+  const divContainer = document.createElement('div')
+  divContainer.style.display = 'flex' //Esto establece el estilo para mostrar los elementos en línea
+  divContainer.appendChild(paragrafhCuatro)  //Agrega el elemento original
+  const paragrafhCinco = document.createElement('p')  //Agrega el nuevo elemento de bloque de texto
+  paragrafhCinco.innerHTML = 'Aunt Christine is warming up the car while Scott is looking for his new leather coat, <br> they are eating at Scott’s favorite restaurant today, Polly’s Pancake Diner.'
+  paragrafhCinco.style.marginLeft = '50px'
+  paragrafhCinco.style.marginTop = '5px'
+  divContainer.appendChild(paragrafhCinco)
+  mainContent.appendChild(divContainer)
 
-
-
-
+  const paragrafhSeis = document.createElement('p')
+  paragrafhSeis.innerHTML = '<br>The present continuous (present progressive) tense is a way to convey any action or condition that is happening right now, frequently, and may be ongoing. It adds energy and action to writing, and its effect helps readers understand when the action is happening. Imagine Aunt Christine has surprised her nephew Scott for his birthday and is going to take him out to his favorite restaurant, Polly’s Pancake Diner. If I wanted to tell the story after it happened, I’d use the past tense.'
+  mainContent.appendChild(paragrafhSeis)
 
 
 }
